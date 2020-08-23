7ed338a41e809604b9f8b60479fcde3eaf5939bc
Caio G. Salomao <caio.gsalomao@outlook.com>
2020-08-23 15:32:50 -0300

    Adicionado PageDefault, cadastro de Categoria e Vídeo. Realizado ajustes no index.css e index.js do src.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import Logo from '../../assets/img/Logo.png'
-import './Menu.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
 import Button from '../Button';
 
 // import ButtonLink from './components/ButtonLink';
@@ -8,13 +9,13 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Logo do CaFlix" />
-            </a>
-            <Button as="a" className="Button" href="/">
+            </Link>
+            <Button as={Link} className="Button" to="/cadastro/video">
                 Novo vídeo 
             </Button>
         </nav>
     )
 }
-export default Menu;
+export default Menu; 
